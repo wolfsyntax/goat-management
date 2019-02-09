01d9a1da49e3a7ff2457745808b5726855e5fdba
Wolf Syntax <wolfsyntax@yahoo.com>
2019-02-10 00:58:55 +0800

    Remove backup directory

--- backup_views/admin/add_account.php
+++
@@ -1,7 +0,0 @@
-<div class="container-fluid">
-	<div class="row">
-		<div class="col">
-			A
-		</div>
-	</div>
-</div>
--- backup_views/include/user_footer.php
+++
@@ -1,3 +0,0 @@
-<div class="navbar-nav fixed-bottom p-2">
-	<?= "<h1>User Footer</h1>";?>
-</div>
